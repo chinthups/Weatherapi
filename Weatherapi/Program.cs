5702650205a1c27ff4b5fcd6ec311600186fba14
saranya chinthu <saranyachinthu@outlook.com>
2023-03-20 11:16:39 +0530

    Add project files.

---
+++ Weatherapi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weatherapi
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://www.weatherapi.com/docs/weather_conditions.json");
+            request.Headers.Add("Cookie", "SERVERUSED=web-3");
+            var response = await client.SendAsync(request);
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+            //response.EnsureSuccessStatusCode();
+            //Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+
+        }
+
+    }
+}
